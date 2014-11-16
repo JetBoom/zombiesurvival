74035790dc5b4b93a9486f7c173e72c03ddd15c8
Ben <b.clark-09@hotmail.com>
2014-11-16 04:54:53 +0000

    Moved CSSWEAPONS from global to local
    There's no reason to have it in globals if it's only used in one place

--- gamemodes/zombiesurvival/gamemode/sh_zombieescape.lua
+++ gamemodes/zombiesurvival/gamemode/sh_zombieescape.lua
@@ -8,6 +8,12 @@ GM.ZE_TimeLimit = 60 * 16
 
 GM.DefaultZombieClass = GM.ZombieClasses["Super Zombie"].Index
 
+local CSSWEAPONS = {"weapon_knife","weapon_glock","weapon_usp","weapon_p228","weapon_deagle",
+	"weapon_elite","weapon_fiveseven","weapon_m3","weapon_xm1014","weapon_galil",
+	"weapon_ak47","weapon_scout","weapon_sg552","weapon_awp","weapon_g3sg1",
+	"weapon_famas","weapon_m4a1","weapon_aug","weapon_sg550","weapon_mac10",
+	"weapon_tmp","weapon_mp5navy","weapon_ump45","weapon_p90","weapon_m249"}
+
 function GM:Move(pl, move)
 	if pl:Team() == TEAM_HUMAN then
 		if pl:GetBarricadeGhosting() then
