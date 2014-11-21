f9c12c63b3c95e098f6192a927692f987b177857
JetBoom <williammoodhe@gmail.com>
2014-11-21 02:24:24 -0500

    Fix typo in GetNailOwner

--- gamemodes/zombiesurvival/gamemode/obj_entity_extend_sv.lua
+++ gamemodes/zombiesurvival/gamemode/obj_entity_extend_sv.lua
@@ -367,7 +367,7 @@ end
 
 local function GetNailOwner(nail, filter)
 	for _, ent in pairs(ents.GetAll()) do
-		if ent and ent ~= filter and ent.Nails and ent:isValid() then
+		if ent and ent ~= filter and ent.Nails and ent:IsValid() then
 			for __, n in pairs(ent.Nails) do
 				if n == nail then
 					return ent
