8637e46990f0072c42cafb8ddbaa1e66d520761f
C0nw0nk <C0nw0nks@googlemail.com>
2015-10-19 20:54:17 +0100

    Update weapon_zs_blaster.lua
    Fix fire rate glitching. (Reloading the gun on 5 shells infinite fireing the weapon as if it was a machine gun.)

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_blaster.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_blaster.lua
@@ -102,7 +102,15 @@ function SWEP:CanPrimaryAttack()
 		return false
 	end
 
-	return true
+	--Start blaster rapid fire glitching fix.
+	if self:GetNextPrimaryFire() + 0.25 > CurTime() then
+		--return false to prevent fireing
+		return false
+	else
+		--return true to allow fireing
+		return true
+	end
+	--End blaster rapid fire glitching fix.
 end
 
 function SWEP:SecondaryAttack()
