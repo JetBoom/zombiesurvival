4d4a2259723ead4eb2172300496cebea878d0662
C0nw0nk <C0nw0nks@googlemail.com>
2015-10-20 18:41:07 +0100

    Update cl_init.lua
    Added name and healthbar to spotlamp.

--- gamemodes/zombiesurvival/entities/entities/prop_spotlamp/cl_init.lua
+++ gamemodes/zombiesurvival/entities/entities/prop_spotlamp/cl_init.lua
@@ -10,6 +10,20 @@ function ENT:SetObjectHealth(health)
 	self:SetDTFloat(0, health)
 end
 
+function ENT:DrawHealthBar(percentage)
+	local y = -50
+	local maxbarwidth = 560
+	local barheight = 30
+	local barwidth = maxbarwidth * percentage
+	local startx = maxbarwidth * -0.5
+
+	surface.SetDrawColor(0, 0, 0, 220)
+	surface.DrawRect(startx, y, maxbarwidth, barheight)
+	surface.SetDrawColor((1 - percentage) * 255, percentage * 255, 0, 220)
+	surface.DrawRect(startx + 4, y + 4, barwidth - 8, barheight - 8)
+	surface.DrawOutlinedRect(startx, y, maxbarwidth, barheight)
+end
+
 local matLight = Material("sprites/light_ignorez")
 function ENT:DrawTranslucent()
 	self:DrawModel()
@@ -39,4 +53,29 @@ function ENT:DrawTranslucent()
 		render.DrawSprite(LightPos, Size, Size, Col, Visibile * ViewDot)
 		render.DrawSprite(LightPos, Size*0.4, Size*0.4, Color(255, 255, 255, Alpha), Visibile * ViewDot)
 	end
+	
+	if MySelf:IsValid() and MySelf:Team() == TEAM_HUMAN then
+		local percentage = math.Clamp(self:GetObjectHealth() / self:GetMaxObjectHealth(), 0, 1)
+		local ang = self:GetAngles()
+		ang:RotateAroundAxis(ang:Up(), 270)
+		ang:RotateAroundAxis(ang:Right(), 180)
+		ang:RotateAroundAxis(ang:Forward(), 180)
+		local vPos = self:GetPos()
+		local vOffset = self:GetForward() * self:OBBMaxs().x
+		local name
+		local owner = self:GetObjectOwner()
+		if owner:IsValid() and owner:IsPlayer() and owner:Team() == TEAM_HUMAN then
+			name = owner:Name()
+		end
+
+		self:DrawModel()
+
+		cam.Start3D2D(vPos + vOffset, ang, 0.05)
+			self:DrawHealthBar(percentage)
+			if name then
+				draw.SimpleText(name, "ZS3D2DFont", 0, 0, COLOR_WHITE, TEXT_ALIGN_CENTER)
+			end
+		cam.End3D2D()
+	end
+	
 end
