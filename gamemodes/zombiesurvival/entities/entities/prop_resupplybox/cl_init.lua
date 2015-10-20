ab52308cca82b7774588450325f7000d19350ef5
C0nw0nk <C0nw0nks@googlemail.com>
2015-10-20 19:04:08 +0100

    Update cl_init.lua

--- gamemodes/zombiesurvival/entities/entities/prop_resupplybox/cl_init.lua
+++ gamemodes/zombiesurvival/entities/entities/prop_resupplybox/cl_init.lua
@@ -55,8 +55,10 @@ function ENT:RenderInfo(pos, ang, owner)
 
 		draw.SimpleText(translate.Get("resupply_box"), "ZS3D2DFont2", 0, 0, NextUse <= CurTime() and COLOR_GREEN or COLOR_DARKRED, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 		
+		if MySelf:IsValid() and MySelf:Team() == TEAM_HUMAN then
 		local percentage = math.Clamp(self:GetObjectHealth() / self:GetMaxObjectHealth(), 0, 1)
 		self:DrawHealthBar(percentage)
+		end
 		
 		if owner:IsValid() and owner:IsPlayer() then
 			draw.SimpleText("("..owner:ClippedName()..")", "ZS3D2DFont2Small", 0, 40, owner == MySelf and COLOR_BLUE or COLOR_GRAY, TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM)
