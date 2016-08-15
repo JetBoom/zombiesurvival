7e7b79464d2c3d6ad3c7563fceacfa8d1c2bb35b
samuelmaddock <samuel.maddock@gmail.com>
2016-08-14 20:53:57 -0400

    Added CS:S collision rules for ZE

--- gamemodes/zombiesurvival/gamemode/sh_zombieescape.lua
+++ gamemodes/zombiesurvival/gamemode/sh_zombieescape.lua
@@ -103,3 +103,40 @@ hook.Add( "PlayerCanPickupWeapon", "RestrictMapWeapons", function( ply, wep )
 		
 	return true
 end)
+
+hook.Add( "ShouldCollide", "CSSShouldCollide", function ( ent1, ent2 )
+
+	-- CS:S Collision Rules
+	local collisionGroup0 = ent1:GetCollisionGroup()
+	local collisionGroup1 = ent2:GetCollisionGroup()
+
+	if collisionGroup0 > collisionGroup1 then
+		local old = collisionGroup0
+		collisionGroup0 = collisionGroup1
+		collisionGroup1 = old
+	end
+
+	if collisionGroup0 == COLLISION_GROUP_PLAYER_MOVEMENT and 
+		collisionGroup1 == COLLISION_GROUP_WEAPON then
+		return false
+	end
+
+	if (collisionGroup0 == COLLISION_GROUP_PLAYER or collisionGroup0 == COLLISION_GROUP_PLAYER_MOVEMENT) and
+		collisionGroup1 == COLLISION_GROUP_PUSHAWAY then
+		return false
+	end
+
+	if collisionGroup0 == COLLISION_GROUP_DEBRIS and collisionGroup1 == COLLISION_GROUP_PUSHAWAY then
+		return true
+	end
+
+	-- No team collisions
+	if ent1:IsPlayer() and ent2:IsPlayer() then
+		return false
+	end
+
+end )
+
+hook.Add( "OnEntityCreated", "CSSCustomCollisions", function( ent )
+	ent:SetCustomCollisionCheck(true)
+end )
