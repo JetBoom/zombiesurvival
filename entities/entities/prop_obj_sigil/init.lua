d6ec589504fbf2eb0f8f69d1f4ed0c043f18048d
MrCraigTunstall <tunstall.craig@yahoo.com>
2018-04-08 21:26:00 +0100

    Fixed end Error

--- entities/entities/prop_obj_sigil/init.lua
+++ entities/entities/prop_obj_sigil/init.lua
@@ -28,23 +28,6 @@ function ENT:Use(activator, caller)
 	local currentSigil = self:GetSigilLetter()
 	local sigils = ents.FindByClass("prop_obj_sigil")
 
-	if IsValid(caller) and caller:IsPlayer() and caller:Team() == TEAM_HUMAN then
-		for _, ent in pairs(sigils) do
-			local sigilIndex = 0
-			local nextSigilIndex = 0
-			if currentSigil == ent:GetSigilLetter() then
-				sigilIndex = _
-				nextSigilIndex = sigilIndex + 1
-			end
-			if sigils[nextSigilIndex] ~= nil then
-				caller:SetPos(sigils[nextSigilIndex]:GetPos())
-				caller:SetBarricadeGhosting(true)
-				self:EmitSound("friends/message.wav")
-				sigils[nextSigilIndex]:EmitSound("friends/friend_online.wav")
-			else
-				if nextSigilIndex ~= 0 then
-					caller:SetPos(sigils[1]:GetPos())
-
 	if IsValid(caller) and caller:IsPlayer() then
 		if caller:Team() ~= TEAM_UNDEAD then
 			for _, ent in pairs(sigils) do
