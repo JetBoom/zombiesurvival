68fed0756e2aab39c69b058fd3cbb11ab8e2d0a0
Flairieve <flairieve@gmail.com>
2018-04-11 13:30:14 -0500

    Updated files

--- entities/entities/status_packup/cl_init.lua
+++ entities/entities/status_packup/cl_init.lua
@@ -34,13 +34,13 @@ function ENT:DrawPackUpBar(x, y, fraction, notowner, screenscale)
 	local barwidth = maxbarwidth * math.Clamp(fraction, 0, 1)
 	local startx = x - maxbarwidth * 0.5
 
-	surface_SetDrawColor(0, 0, 0, 220)
-	surface_DrawRect(startx, y, maxbarwidth, barheight)
-	surface_SetDrawColor(col)
-	surface_DrawRect(startx + 3, y + 3, barwidth - 6, barheight - 6)
-	surface_DrawOutlinedRect(startx, y, maxbarwidth, barheight)
+	surface.SetDrawColor(0, 0, 0, 220)
+	surface.DrawRect(startx, y, maxbarwidth, barheight)
+	surface.SetDrawColor(col)
+	surface.DrawRect(startx + 3, y + 3, barwidth - 6, barheight - 6)
+	surface.DrawOutlinedRect(startx, y, maxbarwidth, barheight)
 
-	draw_SimpleText(notowner and CurTime() % 2 < 1 and translate.Format("requires_x_people", 4) or notowner and translate.Get("packing_others_object") or translate.Get("packing"), "ZSHUDFontSmall", x, y - draw_GetFontHeight("ZSHUDFontSmall") - 2, col, TEXT_ALIGN_CENTER)
+	draw.SimpleText(notowner and CurTime() % 2 < 1 and translate.Format("requires_x_people", 4) or notowner and translate.Get("packing_others_object") or translate.Get("packing"), "ZSHUDFontSmall", x, y - draw.GetFontHeight("ZSHUDFontSmall") - 2, col, TEXT_ALIGN_CENTER)
 end
 
 --local colPackUp = Color(20, 255, 20, 220)
