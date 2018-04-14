ede5666d4d8ef6b8186fd1670cc42b65b52a0217
MrCraigTunstall <tunstall.craig@yahoo.com>
2018-04-14 18:45:01 +0100

    Updated Credits Again

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -25,12 +25,12 @@ GM.Credits = {
 
 GM.Credits2 = {
 	{"MrCraigTunstall", "https://steamcommunity.com/profiles/76561198059515155", "Project Leader"},
+	{"Mka0207", "https://fwkzt.com", "Concept of ZS:R and Zombine Class."}
 	{"Flairieve", "https://steamcommunity.com/profiles/76561198055782802", "Programmer / Contributor"},
 	{"Dadido3", "https://steamcommunity.com/profiles/76561198005024048", "Assistant Programmer / German Translation"},
 	{"Nyanpasu", "https://steamcommunity.com/profiles/76561198158864042", "Issue Tracker / Russian Translation"},
 	{"ForrestMarkX", "https://steamcommunity.com/profiles/76561197997881512", "Spectate System"},
-	{"Sulfito", "https://steamcommunity.com/profiles/76561198026081830", "Spanish Translation"},
-	{"Mka0207", "https://fwkzt.com", "Concept of ZS:R and Zombine Class."}
+	{"Sulfito", "https://steamcommunity.com/profiles/76561198026081830", "Spanish Translation"}
 }
 
 
