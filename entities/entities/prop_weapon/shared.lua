29d22ab73822470a02c202f4bc44baa2bd336904
Mka0207 <g4.tyler@yahoo.com>
2018-04-14 13:41:42 -0500

    Fixed Sandbox Weapon issues.
    Updated prop_weapon to set it's model properly for sandbox weapons.

--- entities/entities/prop_weapon/shared.lua
+++ entities/entities/prop_weapon/shared.lua
@@ -3,12 +3,29 @@ ENT.Base = "prop_baseoutlined"
 
 ENT.NoNails = true
 
+ENT.SandBoxTable = {}
+ENT.SandBoxTable["weapon_crowbar"] = "models/weapons/w_crowbar.mdl" 
+ENT.SandBoxTable["weapon_pistol"] = "models/weapons/w_pistol.mdl" 
+ENT.SandBoxTable["weapon_smg1"] = "models/weapons/w_smg1.mdl" 
+ENT.SandBoxTable["weapon_frag"] = "models/weapons/w_grenade.mdl" 
+ENT.SandBoxTable["weapon_physcannon"] = "models/weapons/w_physics.mdl" 
+ENT.SandBoxTable["weapon_crossbow"] = "models/weapons/w_crossbow.mdl" 
+ENT.SandBoxTable["weapon_shotgun"] = "models/weapons/w_shotgun.mdl" 
+ENT.SandBoxTable["weapon_357"] = "models/weapons/w_357.mdl" 
+ENT.SandBoxTable["weapon_rpg"] = "models/weapons/w_rocket_launcher.mdl" 
+ENT.SandBoxTable["weapon_ar2"] = "models/weapons/w_irifle.mdl" 
+ENT.SandBoxTable["weapon_physgun"] = "models/weapons/w_physics.mdl" 
+ENT.SandBoxTable["weapon_bugbait"] = "models/weapons/w_bugbait.mdl" 
+ENT.SandBoxTable["weapon_stunstick"] = "models/weapons/w_stunbaton.mdl" 
+ENT.SandBoxTable["weapon_slam"] = "models/weapons/w_slam.mdl"
+
 function ENT:HumanHoldable(pl)
 	return pl:KeyDown(GAMEMODE.UtilityKey) or (pl:HasWeapon(self:GetWeaponType()) and self:GetClip1() == 0 and self:GetClip2() == 0)
 end
 
 function ENT:SetWeaponType(class)
 	local weptab = weapons.GetStored(class)
+	local sandbox_tab = self.SandBoxTable
 	if weptab then
 		if weptab.WorldModel then
 			self:SetModel(weptab.WorldModel)
@@ -28,6 +45,10 @@ function ENT:SetWeaponType(class)
 
 		if weptab.ModelScale then
 			self:SetModelScale(weptab.ModelScale, 0)
+		end
+		
+		if self.SandBoxTable[class] then
+			self:SetModel(self.SandBoxTable[string.lower(class)] or "models/weapons/w_bugbait.mdl")
 		end
 	end
 
